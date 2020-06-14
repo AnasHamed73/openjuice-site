6cf4b23a04b5a109dba788c3b07bc842af224d04
Anas Hamed <anas.ta.hamed@gmail.com>
2020-06-14 18:12:17 -0400

    added padding to page headers

--- ojsite/settings.py
+++ ojsite/settings.py
@@ -122,4 +122,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media'),
+)
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
